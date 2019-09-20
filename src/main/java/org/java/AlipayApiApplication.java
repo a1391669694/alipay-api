5314ffdf596d4cd31379b16b94274627b6c916ca
all <huangyongkang@shxgroup.net>
2019-09-20 13:30:58 +0800

    。

---
+++ src/main/java/org/java/AlipayApiApplication.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlipayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlipayApiApplication.class, args);
+    }
+
+}
